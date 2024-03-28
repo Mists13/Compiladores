d0b26ba9645c0e7ed38bbeac60e092d4354faa0e
Vinícius Santos <vods18@inf.ufpr.br>
2024-03-28 15:30:06 -0300

    Update tests and rename main.c to Teste.c

--- Teste.c
+++ Teste.c
@@ -6,9 +6,9 @@ int main ()
     tabela->topo = 0;
     tabela->max = 100;
     tabela->simbolos = malloc(tabela->max * sizeof(char));
-    insere('a', "+ - birulaibe", tabela);
-    insere('b', "sqrt biruleibe", tabela);
-    insere('c', "vrau biruliibe", tabela);
+    insere('a', "VS", "[0, 0, char]", tabela);
+    insere('b', "VS", "[0, 1, char]", tabela);
+    insere('c', "VS", "[0, 2, char]", tabela);
     printf("Atributos encontrados para 'a': %s\n", busca('a', tabela));
     retira(3, tabela);
 }
